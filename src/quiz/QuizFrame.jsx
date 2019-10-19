50cf2c51e9a2daffdbca8e2e1072b802a76cc9fb
marlonkeating <marlonakeating@gmail.com>
2019-10-19 14:33:35 -0700

    Initial commit for safe-injection-quiz-site

---
+++ src/quiz/QuizFrame.jsx
@@ -0,0 +1,13 @@
+// @Flow
+import React from 'react';
+import type {QuizQuestion} from './quizContent.js';
+
+// type Props = {|
+//     quizConfig: 
+// |}
+
+class QuizFrame extends React.PureComponent<Props> {
+
+}
+
+export default QuizFrame;
---
+++ src/quiz/quizContent.js
@@ -0,0 +1,20 @@
+// @Flow
+
+// export type QuizQuestion = {|
+//     question: string,
+//     answers: $ReadOnlyArray<string>,
+//     extendedInfoParagraphs: $ReadOnlyArray<string>,
+// |};
+
+export const quizContent: $ReadOnlyArray<QuizQuestion> = [{
+    question: "This is the question.  What is the answer?",
+    answers: [
+        "42",
+        "Febtober",
+        "African or European Swallow?"
+    ],
+    extendedInfoParagraphs: [
+        "Put any extended information on the subject here"
+        "For the benefit of readers on the answer page"
+    ]
+}];
